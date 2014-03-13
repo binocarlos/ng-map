dee2e95db25e0ecc3e46a365939d003f3feea938
Kai Davenport <kaiyadavenport@gmail.com>
2014-03-13 13:43:40 +0000

    load google lib

--- index.js
+++ index.js
@@ -7,26 +7,7 @@ angular
 
   .directive('mapcanvas', function($http, $window, $q){
     
-    
 
-    function lazyLoadApi(key) {
-      var deferred = $q.defer();
-      function load_script() {
-        deferred.resolve();
-      }
-
-      var s = document.createElement('script');
-      s.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false';
-      document.body.appendChild(s);
-      
-      // thanks to Emil Stenström: http://friendlybit.com/js/lazy-loading-asyncronous-javascript/
-      if ($window.attachEvent) {  
-        $window.attachEvent('onload', load_script); 
-      } else {
-        $window.addEventListener('load', load_script, false);
-      }
-      return deferred.promise;
-    }
     
     return {
       restrict:'EA',
@@ -42,50 +23,60 @@ angular
       link:function($scope, $elem, $attrs){
 
         function attach_map(){
-          
-          var myLatlng = new google.maps.LatLng($scope.lat, $scope.long);
+          var myLatlng = new $window.google.maps.LatLng($scope.lat, $scope.long);
           var mapOptions = {
             zoom: $scope.zoom,
             scrollwheel: false,
             center: myLatlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
           }
-          var map = new google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
+          var map = new $window.google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
 
-          var marker = new google.maps.Marker({
+          var marker = new $window.google.maps.Marker({
               position: myLatlng,
               map: map,
               animation: google.maps.Animation.DROP,
               title: 'Dry Hire UK'
           });
           
-          var contentString = '<div class="info-window-content"><h2>' + $scope.title + '</h2>'+
+          if($scope.title && $scope.title.match(/\w/)){
+            var contentString = '<div class="info-window-content"><h2>' + $scope.title + '</h2>'+
                               '<h3>' + $scope.description + '</h3>';
                               
-          var infowindow = new google.maps.InfoWindow({
-              content: contentString
-          });
+            var infowindow = new $window.google.maps.InfoWindow({
+                content: contentString
+            });
+            
+            google.maps.event.addListener(marker, 'click', function() {
+              infowindow.open(map,marker);
+            });  
+          }
           
-          google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map,marker);
-          });
+          
+        }
+
+        window.gMapsCallback = function(){
+          angular.element($window).trigger('gMapsLoaded');
         }
 
         if ($window.google && $window.google.maps) {
-          console.log('gmaps already loaded');
+          
           attach_map();
         } else {
-          lazyLoadApi().then(function () {
-            console.log('promise resolved');
-            if ($window.google && $window.google.maps) {
-              console.log('gmaps loaded');
-              attach_map();
-            } else {
-              console.log('gmaps not loaded');
-            }
-          }, function () {
-            console.log('promise rejected');
-          });
+
+          function loadGoogleMaps(){
+            var script_tag = document.createElement('script');
+            script_tag.setAttribute("type","text/javascript");
+            script_tag.setAttribute("src","http://maps.google.com/maps/api/js?sensor=false&callback=gMapsCallback");
+            (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+          }
+
+          angular.element($window).bind('gMapsLoaded', function(){
+            attach_map();
+          })
+
+          loadGoogleMaps();
+
         }
 
       }
